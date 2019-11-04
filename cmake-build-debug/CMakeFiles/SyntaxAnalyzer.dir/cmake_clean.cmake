7ff3bf5fc6fc3f3f703e8eeae465d8483ba87c8a
stef4nio <stef4nio@gmail.com>
2019-11-04 23:10:46 +0200

    Initial repository creation. First code added.

---
+++ cmake-build-debug/CMakeFiles/SyntaxAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SyntaxAnalyzer.dir/main.cpp.o"
+  "SyntaxAnalyzer.exe"
+  "SyntaxAnalyzer.pdb"
+  "libSyntaxAnalyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SyntaxAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+
+using namespace std;
+
+void clearAllSpaces(string *str)
+{
+    string resultString = "";
+    for (int i = 0; i < str->length(); ++i)
+    {
+        if(str->at(i)!=' ')
+        {
+            resultString.append(1,str->at(i));
+        }
+    }
+    str->clear();
+    str->append(resultString);
+}
+
+bool isStatement(string input)
+{
+
+}
+
+bool isStatementComparingConstruction(string input)
+{
+    string comparingSigns[]{"==","<",">","<=",">=","!="};
+    bool isFound = false;
+    string stringFound = "";
+    for(int i = 0;i< sizeof(comparingSigns)/ sizeof(string);i++)
+    {
+        if(input.find(comparingSigns[i])!=string::npos)
+        {
+            isFound^= true;
+            stringFound = comparingSigns[i];
+        }
+    }
+    if(isFound)
+    {
+        return isStatement(input.substr(0,input.find(stringFound)))
+        &&isStatement(input.substr(input.find(stringFound)+stringFound.length()-1));
+    }
+}
+
+bool isBoolExpression(string input)
+{
+    if(input.find('(')!=string::npos&&input.find(')')!=string::npos)
+    {
+        int openedBracketIndex = -1;
+        int closedBracketIndex = -1;
+        int openedBracketsAmount = 0;
+        int closedBracketsAmount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input[i] == '(' && openedBracketIndex != -1)
+            {
+
+            }
+        }
+    }
+
+    if(input[0] == '!')
+    {
+        input.erase(0,1);
+        return isBoolExpression(input);
+    }
+
+    if(input == "true" || input == "false")
+    {
+        return true;
+    }
+
+   /* if(input[0]>='0'&&input[0]<='9')
+    {
+        cerr<<"Unallowed variable name."<<endl;
+    }*/
+
+
+    if(isStatementComparingConstruction(input))
+    {
+        return true;
+    }
+
+    if(input.find("||")!=string::npos)
+    {
+        return isBoolExpression(input.substr(0,input.find("||")))&&isBoolExpression(input.substr(input.find("||")+1));
+    }
+    else if(input.find("&&")!=string::npos)
+    {
+        return isBoolExpression(input.substr(0,input.find("&&")))&&isBoolExpression(input.substr(input.find("&&")+1));
+    }
+    else if(input.find("^")!=string::npos)
+    {
+        return isBoolExpression(input.substr(0,input.find("^")))&&isBoolExpression(input.substr(input.find("^")));
+    }
+
+    return false;
+}
+
+bool isIfConstruction(string input)
+{
+    clearAllSpaces(&input);
+    if(input.substr(0,3) == "if(")
+    {
+        input.erase(0,3);
+        if(isBoolExpression(input.substr(0,input.find(')'))))
+        {
+            cout<<"Everything is right";
+            return true;
+        }
+        else
+        {
+            cerr<<"Error recognising your string expected <bool expression>, found "<<input.substr(0,input.find(')'))<<endl;
+            return false;
+        }
+    }
+    else
+    {
+        cerr<<"Error recognising your string. Expected \"if(\", found \""<<input.substr(0,3)<<"\""<<endl;
+        return false;
+    }
+}
+
+int main() {
+    string lol;
+    getline(cin,lol);
+    //clearAllSpaces(&lol);
+    cout<<lol;
+    return 0;
+}
